2a850a6be00ea061a45cc3bc1ef1528f57928fda
Chistaykov Aleksey <chskela@gmail.com>
2022-01-02 16:02:57 +0300

    Move MainActivity in package activities

---
+++ app/src/main/java/com/chskela/shoppinglist/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chskela.shoppinglist.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chskela.shoppinglist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
